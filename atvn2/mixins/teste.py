043693c0e1510938a828f2aa134b9221bb8d5395
igor <igormelo708@gmail.com>
2022-05-06 13:34:33 -0300

    Update teste.py

--- atvn2/mixins/teste.py
+++ atvn2/mixins/teste.py
@@ -2,9 +2,9 @@ from conta import ContaCorrente, SeguroDeVida
 from manipulador import ManipuladorDeTributaveis
 
 cc1 = ContaCorrente('123', 'igor', 900, 1200)
-cc2 = ContaCorrente('123', 'janaina', 1000, 1250)
+cc2 = ContaCorrente('123', 'ellem', 1000, 1250)
 seguro1 = SeguroDeVida(150, 'igor melo', '22334')
-seguro2 = SeguroDeVida(230, 'janaina costa', '55667')
+seguro2 = SeguroDeVida(230, 'ellem jady', '55667')
 
 lista_tributaveis = []
 lista_tributaveis.append(cc1)
